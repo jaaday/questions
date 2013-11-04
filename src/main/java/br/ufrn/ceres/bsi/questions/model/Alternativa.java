f4b3f231ea88ed759f968935e75f3b0bbf1a9032
Jaaday <jaaday.ufrn@gmail.com>
2013-11-03 23:39:42 -0200

    Alterando classes

--- src/main/java/br/ufrn/ceres/bsi/questions/model/Alternativa.java
+++ src/main/java/br/ufrn/ceres/bsi/questions/model/Alternativa.java
@@ -1,6 +1,20 @@
 package br.ufrn.ceres.bsi.questions.model;
 
-public class Alternativa {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({@NamedQuery(name = "Alternativa.allAlternativa", query = "SELECT a FROM Alternativa a"),
+@NamedQuery(name = "Alternativa.countAlternativa", query = "SELECT COUNT(a) FROM Alternativa a")})
+public class Alternativa extends BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(length = 50)
 	private String descricao;
 
 	public Alternativa() {
--- src/main/java/br/ufrn/ceres/bsi/questions/model/Questao.java
+++ src/main/java/br/ufrn/ceres/bsi/questions/model/Questao.java
@@ -1,12 +1,30 @@
 package br.ufrn.ceres.bsi.questions.model;
 
-import java.util.List;
+import java.io.Serializable;
 import java.util.ArrayList;
-import br.ufrn.ceres.bsi.questions.model.Alternativa;
+import java.util.List;
 
-public class Questao {
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+@Entity
+@NamedQueries({@NamedQuery(name = "Questao.allQuestao", query = "SELECT q FROM Questao q"),
+@NamedQuery(name = "Questao.countQuestao", query = "SELECT COUNT(q) FROM Questao q")})
+public class Questao extends BaseEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(length = 50)
 	private String descricao;
+	
+	@OneToMany
 	private List<Alternativa> alternativas = new ArrayList<Alternativa>();
+	
+	@OneToMany
 	private List<Alternativa>  corretas = new ArrayList<Alternativa>();
 	
 	public Questao() {
